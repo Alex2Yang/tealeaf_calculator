d6e6482f3967b256422ea5059553df878ab70e26
Alex Yang <Alex2Yang@users.noreply.github.com>
2015-04-24 13:28:43 +0800

    add two method for get num and calculate, use loop to run this script again.

--- calculator.rb
+++ calculator.rb
@@ -1,37 +1,43 @@
-require 'pry'
-
-def say( msg, newline = true )
-  newline ? print("=> #{msg}\n=> ") : print("=> #{msg}")
+def say(msg)
+  puts "=> #{msg}"
 end
 
-num1, num2 = nil
+def get_num
+  begin
+    say "Enter the number:"
+    num = gets.chomp
+  end while num.empty?
+  num
+end
+
+def get_operator
+  begin
+    say "Operator:1)add 2)sub 3)multiply 4)divide"
+    operator = gets.chomp
+  end while operator.empty?
+  operator
+end
+
+def calculate(n1, operator, n2)
+  if (operator == '4' && n2 == '0')
+    puts "#{n1} cann't be  divided by 0"
+  else
+    result =  case operator
+            when '1' then n1.to_i + n2.to_i
+            when '2' then n1.to_i - n2.to_i
+            when '3' then n1.to_i * n2.to_i
+            when '4' then n1.to_f / n2.to_f
+            end
+    puts "Result is #{result}."
+  end
+end
 
 loop do
-  say "Enter the number:"
-  num1 = gets.chomp
-  break unless  num1.empty?
+  num1 = get_num
+  num2 = get_num
+  operator = get_operator
+  calculate( num1, operator, num2 )
+  say "Once agagin? (yes or no)"
+  ans = gets.chomp
+  break if !ans.empty? && ans.downcase[0] = 'n'
 end
-
-loop do
-  say "Enter the number:"
-  num2 = gets.chomp
-  break unless  num2.empty? ||  num2 == '0'
-end
-
-#binding.pry
-
-say "Operator:1)add 2)sub 3)multiply 4)divide"
-operator = gets.chomp
-
-result = case operator
-         when '1'
-           num1.to_i + num2.to_i
-         when '2'
-           num1.to_i - num2.to_i
-         when '3'
-           num1.to_i * num2.to_i
-         when '4'
-           num1.to_f / num2.to_f
-         end
-
-say "Result is #{result}", false
